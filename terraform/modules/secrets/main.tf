300ff2fd89e438a9f32eacbdd2a1286f31849b8f
Hasan Butt <butthasan65@gmail.com>
2026-08-25 18:57:49 +0500

    feat(secrets): declare epic client id secrets in terraform module

--- terraform/modules/secrets/main.tf
+++ terraform/modules/secrets/main.tf
@@ -12,3 +12,21 @@ resource "google_secret_manager_secret_version" "gemini_api_key_version" {
   secret      = google_secret_manager_secret.gemini_api_key.id
   secret_data = var.gemini_api_key_value
 }
+
+resource "google_secret_manager_secret" "epic_client_id" {
+  secret_id = "${var.prefix}-epic-client-id"
+  project   = var.project_id
+
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret" "epic_nonprod_client_id" {
+  secret_id = "${var.prefix}-epic-nonprod-client-id"
+  project   = var.project_id
+
+  replication {
+    auto {}
+  }
+}
--- terraform/modules/secrets/outputs.tf
+++ terraform/modules/secrets/outputs.tf
@@ -7,3 +7,13 @@ output "gemini_secret_name" {
   description = "Gemini API key Secret Name"
   value       = google_secret_manager_secret.gemini_api_key.name
 }
+
+output "epic_client_id_secret" {
+  description = "Epic Production Client ID Secret Name"
+  value       = google_secret_manager_secret.epic_client_id.name
+}
+
+output "epic_nonprod_client_id_secret" {
+  description = "Epic Non-Production Client ID Secret Name"
+  value       = google_secret_manager_secret.epic_nonprod_client_id.name
+}
